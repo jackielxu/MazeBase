1cce4f7739b65134fbe1784575b3333baf98be2a
Surya Bhupatiraju <surya95@gmail.com>
2016-03-06 23:46:19 -0500

    Use spaces instead of tabs

--- play_game.lua
+++ play_game.lua
@@ -3,40 +3,40 @@ require 'torch'
 dofile('games/init.lua')
 
 function main(gname)
-	g, g_disp, state_win = init_game(gname)
-	while true do
-		print("Waiting...")
-		local res = io.read()
-		local line = string.gsub(res, "\n", "")
-		local action = 5
-		if line == "a" then
-			action = 3
-		elseif line == "d" then
-			action = 4
-		elseif line == "w" then
-			action = 1
-		elseif line == "s" then
-			action = 2
-		else
-			print("Not valid!")
-		end
-		print("Action " .. action .. " pressed!")
-		g:act(action)
-		g_disp.image(g.map:to_image(), {win=state_win})
-	end
-end 
+  g, g_disp, state_win = init_game(gname)
+  while true do
+    print("Waiting...")
+    local res = io.read()
+    local line = string.gsub(res, "\n", "")
+    local action = 5
+    if line == "a" then
+      action = 3
+    elseif line == "d" then
+      action = 4
+    elseif line == "w" then
+      action = 1
+    elseif line == "s" then
+      action = 2
+    else
+      print("Not valid!")
+    end
+    print("Action " .. action .. " pressed!")
+    g:act(action)
+    g_disp.image(g.map:to_image(), {win=state_win})
+  end
+end
 
 function init_game(gname)
-	g_opts = {games_config_path = 'games/config/game_config.lua'}
-	-- Insert the place where we specify what game we want
-	g_init_vocab()
-	g_init_game()
-	g = g_factory:init_game(gname)
-	s = g:to_sentence()
-	g_disp = require('display')
-	state_win = g_disp.image(g.map:to_image())
-	return g, g_disp, state_win
-end 
+  g_opts = {games_config_path = 'games/config/game_config.lua'}
+  -- Insert the place where we specify what game we want
+  g_init_vocab()
+  g_init_game()
+  g = g_factory:init_game(gname)
+  s = g:to_sentence()
+  g_disp = require('display')
+  state_win = g_disp.image(g.map:to_image())
+  return g, g_disp, state_win
+end
 
 local cmd = torch.CmdLine()
 cmd:text()
@@ -48,7 +48,6 @@ cmd:text()
 
 local opt = cmd:parse(arg)
 print(opt)
-gname = opt.gname 
+gname = opt.gname
 
 main(gname)
-
